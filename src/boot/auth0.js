02c5bb29a0ebd9e5fb3b68290daa9f2d87da9cfb
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2025-04-05 04:15:18 -0400

    Update auth0.js

--- src/boot/auth0.js
+++ src/boot/auth0.js
@@ -3,10 +3,10 @@ import { createAuth0 } from "@auth0/auth0-vue";
 export default ({ app }) => {
   app.use(
     createAuth0({
-      domain: "dev-qp4jx4njpfk4xc45.us.auth0.com",
-      clientId: "4BH6oYdvFf4uqZ0s3iEkdDJGZeiCOzFM",
+      domain: "dev-f7hoszot0iiwwsko.us.auth0.com",
+      clientId: "f3xx5JCIUbCKrsQLXZtOfgmKW4RoFlVR",
       authorizationParams: {
-        redirect_uri: "https://hamzehab.github.io/unlimiteddrinks-frontend",
+        redirect_uri: window.location.origin,
       },
     })
   );
